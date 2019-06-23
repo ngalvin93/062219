27a6e8e4d7995bb1011016fa0b26de98de255d20
Alvin Ng <alvinng@Gundam.local>
2019-06-22 22:10:10 -0500

    array exercise completed

--- exercises/106-math.js
+++ exercises/106-math.js
@@ -7,30 +7,43 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "add99" that takes one argument and adds the number 99 to it.
 // You can assume that the argument passed in will be a number value.
-
+function add99 (firstNumber) {
+return firstNumber + 99
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "sum" that takes 2 arguments and sums them together.
 // Assume that both arguments are numbers.
-
+function sum (firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "difference" that takes 2 number arguments and returns their
 // difference.
 // ie: the second number substracted from the first number
-
+function difference (firstNumber, secondNumber) {
+    return firstNumber - secondNumber
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "multiply" that takes 2 number arguments and returns their
 // product.
-
+function multiply (firstNumber, secondNumber) {
+    return firstNumber * secondNumber
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create a function "divide" that takes 2 number arguments and returns their
 // product.
-
+function divide (firstNumber, secondNumber) {
+    return firstNumber / secondNumber
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // The modulus operator (%) works like remainder from division.
 // Create a function "mod" that takes 2 number arguments and returns the
 // remainder of the first divided by the second.
+function mod (firstNumber, secondNumber) {
+    return firstNumber % secondNumber
+}
